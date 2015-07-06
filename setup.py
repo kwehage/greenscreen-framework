fdfb868bdb9587a89c3741b124de4c021fd947b9
Kristopher Wehage <kristopher@wehage.me>
2015-07-06 07:58:25 -0700

    function: updated ghs/greenscreen classes, fixed installation script

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from distutils.core import setup
 
 files = ["data/*"]
 
-setup(name = "greenscreen_framework",
-    version = "100",
-    description = "A collection of utilities for manipulating hazard data and performing Green Screen hazard assessments.",
-    author = "Kristopher Wehage",
-    author_email = "ktwehage@ucdavis.edu",
-    url = "http://amrl.engr.ucdavis.edu",
-    packages = ['greenscreen_framework'],
-    package_data = {'package' : files },
-    long_description = """A collection of utilities for manipulating hazard data and performing Green Screen hazard assessments."""     
-) 
+setup(
+    name="greenscreen_framework",
+    version="1.0.0",
+    description="A collection of utilities for manipulating hazard data " +
+                "and performing Green Screen hazard assessments.",
+    author="Kristopher Wehage",
+    author_email="ktwehage@ucdavis.edu",
+    url="http://amrl.engr.ucdavis.edu",
+    packages=['greenscreen_framework'],
+    py_modules=['greenscreen_framework.ghs',
+                'greenscreen_framework.greenscreen'],
+    package_data={'package': files},
+    long_description="A collection of utilities for manipulating hazard " +
+                     "data and performing Green Screen hazard assessments.")
